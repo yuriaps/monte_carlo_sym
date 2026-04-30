2551ba1d2788bb7075e6f85bf583d88427d08b07
Yuri Lihomanof <yuri.lihomanof@atomoss.com.br>
2026-04-30 19:13:50 +0300

    review

--- monte_carlo.js
+++ monte_carlo.js
@@ -1,5 +1,7 @@
 /* Simulacao Monte Carlo 
    date: 30/04/26
+   Tres portas, A, B, C. Apos o convidado escolher uma, o apresentador abre uma sem premio
+   e o convidado pode escolher trocar ou nao
 */
 
 // Simplificando, assumindo o premio sempre em A
@@ -14,30 +16,30 @@ const ROUNDS = 1000000;
 
 console.log(`## ROUNDS: ${ROUNDS}`);
 
-for (let k = 0; k < ITER; k++){
-    count1 = 0;
-    count2 = 0;
+for (let _k = 0; _k < ITER; _k++){
+    count_no_change = 0;
+    count_w_change = 0;
     // ## caso sem mudar a porta
-    for (let idx = 0; idx< ROUNDS; idx++){
+    for (let _i = 0; _i< ROUNDS; _i++){
         // Escolher uma porta aleatoria dentre as 3
         // Math.floor arredonda pra baixo
         // Math.random() * 3 -> numero nao inteiro de 1 a 3 (na verdade de 0 a 2, pq computacao usa indice 0)
-        const randomItem = data[Math.floor(Math.random() * 3)];
-        if (randomItem.prize === true) count1++;
+        const random_door = data[Math.floor(Math.random() * 3)];
+        if (random_door.prize === true) count_no_change++;
 
     }
     // ## caso mudando sempre de porta
-    for (let idx = 0; idx < ROUNDS; idx++){
+    for (let _i = 0; _i < ROUNDS; _i++){
         // Porta aleatoria entre as 3
-        const randomItem = data[Math.floor(Math.random() * 3)];
-        // Filtrar sempre deixando "A"
-        const data_after_open = filter_data(randomItem);
+        const random_door = data[Math.floor(Math.random() * 3)];
+        // Filtrar data sempre deixando "A" e "B" ou "A" e "C", dependendo do item 
+        const data_after_open = filter_data(random_door);
         // Trocar
-        const final_door = change(randomItem, data_after_open); 
-        if (final_door.prize === true) count2++;
+        const final_door = change(random_door, data_after_open); 
+        if (final_door.prize === true) count_w_change++;
         
     }
-    console.log(`ITER: ${k+1}/${ITER}, p_sem_mudar: ${count1/ROUNDS}, p_mudando_sempre: ${count2/ROUNDS}`);
+    console.log(`ITER: ${_k+1}/${ITER}, p_sem_mudar: ${count_no_change/ROUNDS}, p_mudando_sempre: ${count_w_change/ROUNDS}`);
 }
 
 function filter_data(door){
@@ -72,4 +74,18 @@ function change(door, data_input){
     }
 }
 
-
+/*
+resultado:
+-----------------------------------------------------------
+## ROUNDS: 1000000
+ITER: 1/10, p_sem_mudar: 0.333113, p_mudando_sempre: 0.666247
+ITER: 2/10, p_sem_mudar: 0.333953, p_mudando_sempre: 0.667407
+ITER: 3/10, p_sem_mudar: 0.332743, p_mudando_sempre: 0.666661
+ITER: 4/10, p_sem_mudar: 0.333571, p_mudando_sempre: 0.666665
+ITER: 5/10, p_sem_mudar: 0.333143, p_mudando_sempre: 0.667262
+ITER: 6/10, p_sem_mudar: 0.333449, p_mudando_sempre: 0.6673
+ITER: 7/10, p_sem_mudar: 0.333406, p_mudando_sempre: 0.667371
+ITER: 8/10, p_sem_mudar: 0.333466, p_mudando_sempre: 0.666218
+ITER: 9/10, p_sem_mudar: 0.333601, p_mudando_sempre: 0.666049
+ITER: 10/10, p_sem_mudar: 0.334013, p_mudando_sempre: 0.667082
+*/
